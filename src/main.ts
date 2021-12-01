6819a10a0c39da6900f6036c7cf0533a22ff0152
hardhill <hardhill@mail.ru>
2021-12-01 23:32:00 +0800

    ready

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import Ball from "./classes/Ball"
+import Mouse from "./classes/Mouse"
+
+
+let canvas:HTMLCanvasElement = document.getElementById('drawField') as any
+const ctx:CanvasRenderingContext2D|null = canvas.getContext('2d')
+   
+const mouse = new Mouse(canvas)
+const balls:Array<Ball> = new Array<Ball>()
+for(let i=0;i<600;i++){
+    let x = Math.random()*600
+    let y = Math.random()*600
+    let ball = new Ball(ctx!,x,y,3,'#aa6600')
+    balls.push(ball)
+}
+const cursor = new Ball(ctx!,0,0,30,'#22cc1933')
+Render()
+
+
+
+function Render(){
+    window.requestAnimationFrame(Render)
+    ctx!.clearRect(0,0,600,600)
+
+    balls.forEach(item=>{
+        item.draw()
+        item.think(mouse.x, mouse.y)
+    })
+    cursor.setPos(mouse.x,mouse.y)
+    cursor.draw()
+    
+}
+
